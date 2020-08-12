1d45bcae36441eb82150478336f8d9fe3c49045d
Eugene O'Neil <ejo7220@gmail.com>
2020-08-12 19:08:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,9 @@
 class Triangle
   attr_accessor :x, :y, :z
-  initialize(x, y, z)
-  @x = x
-  @y = y 
-  @z = z 
-end
+   def initialize(x, y, z)
+      @x = x
+      @y = y 
+      @z = z 
+   end
 
 end
